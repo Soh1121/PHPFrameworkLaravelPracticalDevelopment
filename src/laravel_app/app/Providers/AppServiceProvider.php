cead7f582cc0de7857ea1a48301c87e6d004337b
Soh1121 <satou.shg@gmail.com>
2021-12-13 17:00:06 +0900

    [update]結合時のイベント処理について学習
    バインド時のイベント処理を組み込むため

--- src/laravel_app/app/Providers/AppServiceProvider.php
+++ src/laravel_app/app/Providers/AppServiceProvider.php
@@ -24,6 +24,21 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        app()->bind('App\MyClasses\MyServiceInterface', 'App\MyClasses\PowerMyService');
+        app()->resolving(function ($obj, $app) {
+            if (is_object($obj)) {
+                echo get_class($obj) . '<br>';
+            } else {
+                echo $obj . '<br>';
+            }
+        });
+
+        app()->resolving(PowerMyService::class, function ($obj, $app) {
+            echo "in";
+            $newdata = ['ハンバーグ', 'カレーライス', '唐揚げ', '餃子'];
+            $obj->setData($newdata);
+            $obj->setId(rand(0, count($newdata)));
+        });
+
+        app()->singleton('App\MyClasses\MyServiceInterface', 'App\MyClasses\PowerMyService');
     }
 }
