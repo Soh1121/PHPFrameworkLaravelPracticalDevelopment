dc7b226a900552aeb302459b0d4d6c8a68d1d51e
Soh1121 <satou.shg@gmail.com>
2021-12-13 10:55:22 +0900

    [update]サービスコンテナへの結合方法を学習
    インスタンスをサービスコンテナに結合するため

--- src/laravel_app/app/Http/Controllers/HelloController.php
+++ src/laravel_app/app/Http/Controllers/HelloController.php
@@ -2,11 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use App\MyClasses\MyService;
+
 class HelloController extends Controller
 {
-    public function index(int $id = -1)
+    public function index(MyService $myservice, int $id = -1)
     {
-        $myservice = app()->makeWith('App\MyClasses\MyService', ['id' => $id]);
+        $myservice->setId($id);
         $data = [
             'msg' => $myservice->say(),
             'data' => $myservice->alldata()
--- src/laravel_app/routes/web.php
+++ src/laravel_app/routes/web.php
@@ -18,4 +18,5 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/hello/{id}', 'HelloController@index')->name('hello');
+Route::get('/hello', 'HelloController@index')->name('hello');
+Route::get('/hello/{id}', 'HelloController@index');
