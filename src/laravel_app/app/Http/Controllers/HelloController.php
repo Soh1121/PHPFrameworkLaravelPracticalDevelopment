3b0819bfbc67f609df2a9ae04725e6493440c376
Soh1121 <satou.shg@gmail.com>
2021-12-13 10:40:20 +0900

    [update]明示的にインスタンスを生成する方法を学習
    メソッド内でコードを記述して必要なインスタンスを取得したい場合のため

--- src/laravel_app/app/Http/Controllers/HelloController.php
+++ src/laravel_app/app/Http/Controllers/HelloController.php
@@ -2,12 +2,11 @@
 
 namespace App\Http\Controllers;
 
-use App\MyClasses\MyService;
-
 class HelloController extends Controller
 {
-    public function index(MyService $myservice)
+    public function index()
     {
+        $myservice = app('App\MyClasses\MyService');
         $data = [
             'msg' => $myservice->say(),
             'data' => $myservice->data()
