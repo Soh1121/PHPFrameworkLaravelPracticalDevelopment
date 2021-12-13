ed49925556d2c1783e9944be8bc519fbb3a13d51
Soh1121 <satou.shg@gmail.com>
2021-12-13 10:46:53 +0900

    [update]引数が必要なクラスのインスタンス取得方法を学習
    インスタンス生成に引数が必要なときのため

--- src/laravel_app/app/Http/Controllers/HelloController.php
+++ src/laravel_app/app/Http/Controllers/HelloController.php
@@ -4,12 +4,12 @@ namespace App\Http\Controllers;
 
 class HelloController extends Controller
 {
-    public function index()
+    public function index(int $id = -1)
     {
-        $myservice = app('App\MyClasses\MyService');
+        $myservice = app()->makeWith('App\MyClasses\MyService', ['id' => $id]);
         $data = [
             'msg' => $myservice->say(),
-            'data' => $myservice->data()
+            'data' => $myservice->alldata()
         ];
         return view('hello.index', $data);
     }
