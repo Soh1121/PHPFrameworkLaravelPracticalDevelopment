1c58e95b94a32b26abac2d2ba67cebad067a84a8
Soh1121 <satou.shg@gmail.com>
2021-12-16 13:33:48 +0900

    [update]Personモデルの基本形を学習
    Eloquentを利用するため

--- src/laravel_app/resources/views/hello/index.blade.php
+++ src/laravel_app/resources/views/hello/index.blade.php
@@ -20,13 +20,16 @@
 <body>
 	<h1>Hello/Index</h1>
 	<p>{{$msg}}</p>
-	<ol>
+	<table border="1">
 		@foreach($data as $item)
-		<li>{{ $item->name }} [{{$item->mail}}, {{ $item->age }}]</li>
+		<tr>
+			<th>{{ $item->id }}</th>
+			<td>{{ $item->name }}</td>
+			<td>{{ $item->mail }}</td>
+			<td>{{ $item->age }}</td>
+		</tr>
 		@endforeach
-	</ol>
-	<hr>
-	{!! $paginator->link() !!}
+	</table>
 </body>
 
 </html>
