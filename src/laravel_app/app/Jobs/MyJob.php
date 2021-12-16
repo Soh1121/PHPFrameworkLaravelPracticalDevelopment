4b10ac288e34a911ec8fd2d816539de954a92752
Soh1121 <satou.shg@gmail.com>
2021-12-16 21:14:57 +0900

    [add]ジョブを作成する方法を学習
    キュートジョブを用いるため

---
+++ src/laravel_app/app/Jobs/MyJob.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Jobs;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+
+class MyJob implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    public function __construct()
+    {
+        //
+    }
+
+    public function handle()
+    {
+        echo '<p class="myjob">THIS IS MYJOB!</p>';
+    }
+}
