83b636ad8fa94cb26d66634d848cf6aee4504f3d
Soh1121 <satou.shg@gmail.com>
2021-12-20 16:14:43 +0900

    [update]イベントディスカバリについて学習
    イベントとリスナーを自動で登録するため

--- src/laravel_app/app/Providers/EventServiceProvider.php
+++ src/laravel_app/app/Providers/EventServiceProvider.php
@@ -2,27 +2,19 @@
 
 namespace App\Providers;
 
-use Illuminate\Support\Facades\Event;
-use Illuminate\Auth\Events\Registered;
-use Illuminate\Auth\Listeners\SendEmailVerificationNotification;
 use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
 
 class EventServiceProvider extends ServiceProvider
 {
-    protected $listen = [
-        Registered::class => [
-            SendEmailVerificationNotification::class,
-        ],
-    ];
-
-    protected $subscribe = [
-        'App\Listeners\MyEventSubscriber',
-    ];
-
     public function boot()
     {
         parent::boot();
 
         //
+    }
+
+    public function shouldDiscoverEvents()
+    {
+        return true;
     }
 }
