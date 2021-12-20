efe211e823815dc8f4483b385930fad210e36e78
Soh1121 <satou.shg@gmail.com>
2021-12-20 15:35:24 +0900

    [update]PersonEventについて学習
    Eventを作成するため

--- src/laravel_app/app/Events/PersonEvent.php
+++ src/laravel_app/app/Events/PersonEvent.php
@@ -2,35 +2,17 @@
 
 namespace App\Events;
 
-use Illuminate\Broadcasting\Channel;
+use App\Person;
 use Illuminate\Queue\SerializesModels;
-use Illuminate\Broadcasting\PrivateChannel;
-use Illuminate\Broadcasting\PresenceChannel;
-use Illuminate\Foundation\Events\Dispatchable;
-use Illuminate\Broadcasting\InteractsWithSockets;
-use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
 
 class PersonEvent
 {
-    use Dispatchable, InteractsWithSockets, SerializesModels;
+    use SerializesModels;
 
-    /**
-     * Create a new event instance.
-     *
-     * @return void
-     */
-    public function __construct()
-    {
-        //
-    }
+    public $person;
 
-    /**
-     * Get the channels the event should broadcast on.
-     *
-     * @return \Illuminate\Broadcasting\Channel|array
-     */
-    public function broadcastOn()
+    public function __construct(Person $person)
     {
-        return new PrivateChannel('channel-name');
+        $this->person = $person;
     }
 }
