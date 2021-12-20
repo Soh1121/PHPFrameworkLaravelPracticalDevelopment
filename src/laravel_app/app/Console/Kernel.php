d97d49522bfb4c6fb1f06cd74d0d0cb2ce3c423e
Soh1121 <satou.shg@gmail.com>
2021-12-20 16:48:39 +0900

    [update]invoke実装クラスをcallする方法を学習
    オブジェクトを引数にcallするため

--- src/laravel_app/app/Console/Kernel.php
+++ src/laravel_app/app/Console/Kernel.php
@@ -6,43 +6,46 @@ use App\Jobs\MyJob;
 use App\Person;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
+use Illuminate\Support\Facades\Storage;
 
 class Kernel extends ConsoleKernel
 {
-    /**
-     * The Artisan commands provided by your application.
-     *
-     * @var array
-     */
     protected $commands = [
         //
     ];
 
-    /**
-     * Define the application's command schedule.
-     *
-     * @param  \Illuminate\Console\Scheduling\Schedule  $schedule
-     * @return void
-     */
     protected function schedule(Schedule $schedule)
     {
         $count = Person::all()->count();
         $id = rand(0, $count) + 1;
-        $schedule->call(function () use ($id) {
-            $person = Person::find($id);
-            MyJob::dispatch($person);
-        });
+        $obj = new ScheduleObj($id);
+        $schedule->call($obj);
     }
 
-    /**
-     * Register the commands for the application.
-     *
-     * @return void
-     */
     protected function commands()
     {
         $this->load(__DIR__ . '/Commands');
 
         require base_path('routes/console.php');
+    }
+}
+
+class ScheduleObj
+{
+    private $person;
+
+    public function __construct($id)
+    {
+        $this->person = Person::find($id);
+    }
+
+    public function __invoke()
+    {
+        Storage::append(
+            'person_access_log.txt',
+            $this->person->all_data
+        );
+        MyJob::dispatch($this->person);
+        return 'true';
     }
 }
