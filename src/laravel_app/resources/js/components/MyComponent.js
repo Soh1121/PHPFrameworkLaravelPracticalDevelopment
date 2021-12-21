1304f59a8db6788959193a7244f10aa3b33c0554
Soh1121 <satou.shg@gmail.com>
2021-12-21 17:11:38 +0900

    [update]クライアント＝サーバー通信について学習
    LaravelとReactの関係性と実装について

--- src/laravel_app/resources/js/components/MyComponent.js
+++ src/laravel_app/resources/js/components/MyComponent.js
@@ -9,14 +9,29 @@ export default class MyComponent extends Component {
 			msg: 'ok',
 		};
 		this.doChange = this.doChange.bind(this);
+		this.doAction = this.doAction.bind(this);
 	}
 
 	doChange(event) {
 		let n = event.target.value;
 		this.setState((state)=>({
 			num: n,
-			msg: 'count: ' + n,
+			person: null,
 		}));
+	}
+	doAction(event) {
+		this.setState((state)=>({
+			msg: 'wait...',
+		}));
+		axios.get('/hello/json/' + this.state.num)
+			.then(response => {
+				let person = response.data;
+				let msg = person.id + ':' + person.name + ' [' + person.mail + '] (' + person.age + ')';
+				this.setState((state)=>({
+					person: person,
+					msg: msg
+				}));
+			});
 	}
 
 	render() {
@@ -25,6 +40,7 @@ export default class MyComponent extends Component {
 				<p>{ this.state.msg }</p>
 				<div>
 					<input type="number" id="num" onChange={this.doChange} />
+					<button onClick={this.doAction}>Click</button>
 				</div>
 			</div>
 		);
