aa9ea8fe53dfa30ec901cd0cf2207ae19661ed97
Soh1121 <satou.shg@gmail.com>
2021-12-21 18:57:03 +0900

    [update]モデルのテストを行う方法を学習
    モデルをテストするため

--- src/laravel_app/tests/Feature/ExampleTest.php
+++ src/laravel_app/tests/Feature/ExampleTest.php
@@ -9,21 +9,14 @@ class ExampleTest extends TestCase
 {
     public function testBasicTest()
     {
-        $this->get('/')->assertStatus(200);
-        $this->get('/hello')->assertOk();
-        // $this->post('/hello')->assertOk();
-        $this->get('/hello/1')->assertOk();
-        $this->get('/hoge')->assertStatus(404);
-        $this->get('/hello')->assertSeeText('Index');
-        $this->get('/hello')->assertSee('<h1>');
-        $this->get('/hello')->assertSeeInOrder(['<html', '<head', '<body', '<h1>']);
-        $this->get('/hello/json/1')->assertSeeText('TARO');
-        // $this->get('/hello/json/2')->assertExactJson(
-        //     [
-        //         'id' => 2, 'name' => 'HANAKO [+MYJOB]',
-        //         'mail' => 'hanako@flower.jp', 'age' => 34, 'created_at' => null,
-        //         'updated_ad' => '2021-12-20 06:14:56'
-        //     ]
-        // );
+        $data = [
+            'id' => 1,
+            'name' => 'YAMADA-TARO',
+            'mail' => 'taro@yamada',
+            'age' => 34
+        ];
+        $this->assertDatabaseHas('people', $data);
+        $data['id'] = 2;
+        $this->assertDatabaseMissing('people', $data);
     }
 }
