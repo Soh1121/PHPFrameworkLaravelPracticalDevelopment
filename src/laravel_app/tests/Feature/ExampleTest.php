695768076b2cb5fdf1198dff00d9d29dcf6008a7
Soh1121 <satou.shg@gmail.com>
2021-12-21 20:55:55 +0900

    [update]ステートを設定する方法を学習
    設定する値を制御するため

--- src/laravel_app/tests/Feature/ExampleTest.php
+++ src/laravel_app/tests/Feature/ExampleTest.php
@@ -13,17 +13,26 @@ class ExampleTest extends TestCase
 
     public function testBasicTest()
     {
-        for ($i = 0; $i < 100; $i++) {
-            factory(Person::class)->create();
+        $list = [];
+        for ($i = 0; $i < 10; $i++) {
+            $p1 = factory(Person::class)->create();
+            $p2 = factory(Person::class)->states('upper')->create();
+            $p3 = factory(Person::class)->states('lower')->create();
+            $p4 = factory(Person::class)->states('upper')->state('lower')->create();
+            $list = array_merge($list, [$p1->id, $p2->id, $p3->id, $p4->id]);
         }
-        $count = Person::get()->count();
-        $person = Person::find(rand(1, $count));
-        $data = $person->toArray();
-        print_r($data);
 
-        $this->assertDatabaseHas('people', $data);
+        for ($i = 0; $i < 10; $i++) {
+            shuffle($list);
+            $item = array_shift($list);
+            $person = Person::find($item);
+            $data = $person->toArray();
+            print_r($data);
 
-        $person->delete();
-        $this->assertDatabaseMissing('people', $data);
+            $this->assertDatabaseHas('people', $data);
+
+            $person->delete();
+            $this->assertDatabaseMissing('people', $data);
+        }
     }
 }
