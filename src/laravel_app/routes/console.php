c2dc9f2cac665735b3374c3a3f0a813d8f7c7b67
Soh1121 <satou.shg@gmail.com>
2021-12-22 17:27:17 +0900

    [update]クロージャコマンドについて学習
    Commandクラスを使わないコマンド作成について学ぶため

--- src/laravel_app/routes/console.php
+++ src/laravel_app/routes/console.php
@@ -16,3 +16,10 @@ use Illuminate\Foundation\Inspiring;
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->describe('Display an inspiring quote');
+
+Artisan::command('person {id}', function () {
+    $id = $this->argument('id');
+    $p = App\Person::find($id);
+    $this->question('id = $id');
+    $this->line($p->all_data());
+});
