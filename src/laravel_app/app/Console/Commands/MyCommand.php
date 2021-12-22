e57a2c081c6a3d7443ec94252355a7bfb50ea245
Soh1121 <satou.shg@gmail.com>
2021-12-22 13:30:49 +0900

    [update]シグネチャと説明を用意
    コマンド開発の第一歩のため

--- src/laravel_app/app/Console/Commands/MyCommand.php
+++ src/laravel_app/app/Console/Commands/MyCommand.php
@@ -6,35 +6,15 @@ use Illuminate\Console\Command;
 
 class MyCommand extends Command
 {
-    /**
-     * The name and signature of the console command.
-     *
-     * @var string
-     */
-    protected $signature = 'command:name';
+    protected $signature = 'my:cmd';
 
-    /**
-     * The console command description.
-     *
-     * @var string
-     */
-    protected $description = 'Command description';
+    protected $description = 'This is my first command!';
 
-    /**
-     * Create a new command instance.
-     *
-     * @return void
-     */
     public function __construct()
     {
         parent::__construct();
     }
 
-    /**
-     * Execute the console command.
-     *
-     * @return mixed
-     */
     public function handle()
     {
         //
