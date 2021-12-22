962cfa49bf31570b00f6151979b438dbbe4afaed
Soh1121 <satou.shg@gmail.com>
2021-12-22 13:32:27 +0900

    [update]コマンドの出力を作成するhandleメソッドを学習
    コマンドを実行するため

--- src/laravel_app/app/Console/Commands/MyCommand.php
+++ src/laravel_app/app/Console/Commands/MyCommand.php
@@ -3,6 +3,7 @@
 namespace App\Console\Commands;
 
 use Illuminate\Console\Command;
+use Illuminate\Foundation\Inspiring;
 
 class MyCommand extends Command
 {
@@ -17,6 +18,8 @@ class MyCommand extends Command
 
     public function handle()
     {
-        //
+        echo "\n*今日の格言*\n\n";
+        echo Inspiring::quote();
+        echo "\n\n";
     }
 }
